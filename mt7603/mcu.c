0c579cfd43bb81e1d469ed42d34468325e3d6a6d
Gedankenn <gedankenn@gmail.com>
2022-01-11 09:18:14 -0300

    Printk na função de set tx power

--- mt7603/mcu.c
+++ mt7603/mcu.c
@@ -340,6 +340,7 @@ int mt7603_mcu_set_eeprom(struct mt7603_dev *dev)
 
 static int mt7603_mcu_set_tx_power(struct mt7603_dev *dev)
 {
+	printk("Entra na função de setar o TX POWER\n");
 	struct {
 		u8 center_channel;
 		u8 tssi;
@@ -380,6 +381,7 @@ static int mt7603_mcu_set_tx_power(struct mt7603_dev *dev)
 
 	return mt76_mcu_send_msg(&dev->mt76, MCU_EXT_CMD_SET_TX_POWER_CTRL,
 				 &req, sizeof(req), true);
+	printk("Target_power: %d , %d\n",req.target_power[1], req.target_power[2]);
 }
 
 int mt7603_mcu_set_channel(struct mt7603_dev *dev)
