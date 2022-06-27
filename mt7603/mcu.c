6aa9eded899d12e0907734500353b43b7f0a7fe1
Gedankenn <gedankenn@gmail.com>
2022-06-27 16:34:07 -0300

    teste

--- mt7603/mcu.c
+++ mt7603/mcu.c
@@ -410,10 +410,10 @@ int mt7603_mcu_set_channel(struct mt7603_dev *dev)
 		.tx_streams = n_chains,
 		.rx_streams = n_chains,
 	};
-	s8 tx_power = hw->conf.power_level * 6;
+	s8 tx_power = hw->conf.power_level * 12;
 	int i, ret;
 	
-	tx_power = 27;
+	tx_power = 54;
 
 	if (dev->mphy.chandef.width == NL80211_CHAN_WIDTH_40) {
 		req.bw = MT_BW_40;
@@ -427,7 +427,7 @@ int mt7603_mcu_set_channel(struct mt7603_dev *dev)
 	if (dev->mphy.antenna_mask == 3)
 		tx_power -= 6;
 	// tx_power = min(tx_power, dev->tx_power_limit);
-	tx_power = 27;
+	tx_power = 54;
 
 	dev->mphy.txpower_cur = tx_power;
 
